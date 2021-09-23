fa4462fe5493d6ab01566aa43dc992ea50dd8a49
matheuscal <matheuscalderon@hotmail.com>
2021-09-22 20:51:35 -0400

    added custom site title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Portfolio</title>
+    <title><?php echo get_bloginfo('name'); ?></title>
     <?php wp_head() ?>
 </head>
 <body>
